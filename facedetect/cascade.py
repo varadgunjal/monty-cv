3b1a632da178d3ee30da3ea5cd83c6e04f7b6ce4
varad <varad@aquarius.com>
2016-09-19 21:11:21 +0530

    haarcascade face detection

--- facedetect/cascade.py
+++ facedetect/cascade.py
@@ -1,4 +1,12 @@
 import cv2
 
-def cascadeClassifier():
-    image = cv2.imread(input_image)
+def cascadeClassifier(input_file):
+    image = cv2.imread(input_file)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # detect faces
+    detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+    rects = detector.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=7,
+	                    minSize=(30, 30), flags=cv2.cv.CV_HAAR_SCALE_IMAGE)
+
+    return rects
